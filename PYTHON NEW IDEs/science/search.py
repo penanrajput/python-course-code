06637cb8996b3a321f3a5890b03e5f6b48df12c7
Penan Rajput <penanrajput1998@gmail.com>
2019-06-16 03:27:26 +0530

    16 June 2019 3:27 AM = new Content

--- PYTHON NEW IDEs/science/search.py
+++ PYTHON NEW IDEs/science/search.py
@@ -1,7 +1,16 @@
-import random
-arr = []
-for i in range(5):
-    r = random.randint(0,120)
-    if r > 70 :
-        arr.append(r)
-print(arr)
+# import random
+# arr = []
+# for i in range(5):
+#     r = random.randint(0,120)
+#     if r > 70 :
+#         arr.append(r)
+# print(arr)
+
+num = '60/70/80/90'
+num=num.spli╤t('/')
+list = []
+
+for item in num:
+    list.append(int(item))
+
+print(list)
