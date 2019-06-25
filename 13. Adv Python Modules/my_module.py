75d7c075b96a2776ddd92ba17d19957c53435a56
Penan Rajput <penanrajput1998@gmail.com>
2019-06-25 08:49:38 +0530

    modules done

---
+++ 13. Adv Python Modules/my_module.py
@@ -0,0 +1,9 @@
+# this is my_module.py file
+
+def internal():
+    '''Docstring of internal function: pass no argument and no return'''
+    print("This is internal function")
+
+def _external():
+    '''Docstring of _external function: pass no argument and no return'''
+    print("This is external function")
---
+++ 13. Adv Python Modules/mymodule03.py
@@ -0,0 +1,8 @@
+def summation(a, b):
+    '''this is Docstring. It doesn't do anything'''
+    print( a+b)
+
+def subst (a, b):
+    print(f'{a}-{b}={a-b}' )
+if(__name__=='__main__'):
+    print("mymodule is run")
